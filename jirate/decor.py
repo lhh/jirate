0efbf68fefdb9c027b59dff001de416dc7359db0
Lon Hohberger <lon@metamorphism.com>
2024-09-17 14:43:39 -0400

    decor: truncate field name to max width of field values to save horizontal space
    Some field names have considerably longer names than the values they contain.
    For example, "Story Points" is usually a numerical value with the longest value
    being unset ("None").  Rendering a matrix like the following (fields:
    "story_points,summary"):
    
      key    | story_points | summary
      TEST-1 | 1            | test
      TEST-2 | 11           | test2
    
    This is a waste of horizontal screen real estate.  Instead, truncate the field
    name width to the maximum value of the column to save space:
    
      key    | s… | summary
      TEST-1 | 1  | test
      TEST-2 | 11 | test2
    
    This also changes a single-character width from '…' to the first character of
    the field name:
    
      key    | s | summary
      TEST-1 | 1 | test

--- jirate/decor.py
+++ jirate/decor.py
@@ -153,6 +153,8 @@ def comma_separated(item_list):
 
 def truncate(arg, maxlen):
     if arg and maxlen:
+        if maxlen == 1:
+            return str(arg[0])
         if maxlen > 0 and len(arg) > maxlen:
             arg = arg[:maxlen - 1] + '…'
         if maxlen < 0 and len(arg) > abs(maxlen):
@@ -360,13 +362,17 @@ def vsep_print(linesplit=None, screen_width=0, *vals):
     return screen_width
 
 
-def render_matrix(matrix, header=True, header_bar=True):
+def render_matrix(matrix, header=True, header_bar=True, trunc_headers=True):
     screen_width = shutil.get_terminal_size()[0]
     # Renders a table with the right-most field truncated/wrapped if needed.
     # Undefined if the screen width is too wide to accommodate all but the
     # last field
     col_widths = len(matrix[0]) * [0]
-    for row in matrix:
+    if trunc_headers:
+        start_row = 1
+    else:
+        start_row = 0
+    for row in matrix[start_row:]:
         if len(row) != len(col_widths):
             raise ValueError('Column count mismatch')
         for val in range(0, len(col_widths)):
@@ -379,7 +385,9 @@ def render_matrix(matrix, header=True, header_bar=True):
         start = 1
         line = []
         for item in range(0, len(col_widths)):
-            line.extend([matrix[0][item], col_widths[item]])
+            # We truncate here to max length from field array
+            val = truncate(matrix[0][item], col_widths[item])
+            line.extend([val, col_widths[item]])
         line.pop()
         # XXX Do we want to render full-width here?
         width = vsep_print(' ', screen_width, *line)
