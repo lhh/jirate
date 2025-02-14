6e5c66405e0c3b875d4a4fb414cadf555a7b386a
Lon Hohberger <lon@metamorphism.com>
2025-02-14 09:28:53 -0500

    jira: Work around user() missing feature
    In pyjira 3.8.0, JIRA.user() searches by username,
    but some things (like EAUSM-vote) return the key value.
    
    Wrap JIRA.user() to try key if the others don't exist.
    
    Provide a sketchy wrapper we can use to search by key
    to avoid double-API calls.

--- jirate/rqcache.py
+++ jirate/rqcache.py
@@ -37,7 +37,8 @@ default_cache_patterns = {
             r'/rest/agile/[0-9]+(\.[0-9]+)?/board$',
             r'/rest/agile/[0-9]+(\.[0-9]+)?/board/[0-9]+/sprint$',
             r'/rest/api/[0-9]+/project/[A-Z]+/statuses$',
-            r'/rest/api/[0-9]+/project/[A-Z]+$']
+            r'/rest/api/[0-9]+/project/[A-Z]+$',
+            r'/rest/api/[0-9]+/user\?(username|key)=']
 }
 
 
